3b5c08057d19162f6caa13e0cd4b3b133961ba46
enzotiezzi <enzo_tiezzi@hotmail.com>
2016-12-04 00:21:28 -0200

    tirando angular2

--- public/app/main.ts
+++
@@ -1,5 +0,0 @@
-///<reference path="../../node_modules/typescript/lib/lib.es6.d.ts"/> 
-
-import { bootstrap }    from '@angular/platform-browser-dynamic';
-import { AppComponent } from './app.component';
-bootstrap(AppComponent);
