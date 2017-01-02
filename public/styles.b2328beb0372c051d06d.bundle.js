fa9516ef41a9a2441212c86027a1b4af56d396c3
enzotiezzi <enzo_tiezzi@hotmail.com>
2017-01-02 19:59:19 -0200

    colocando angular 2

---
+++ public/styles.b2328beb0372c051d06d.bundle.js
@@ -0,0 +1,2 @@
+webpackJsonp([2,4],{389:function(n,o){},723:function(n,o,c){n.exports=c(389)}},[723]);
+//# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
--- server.js
+++ server.js
@@ -33,8 +33,8 @@ app.use(cors());
 // usa rotas
 app.use('/', home);
 app.use('/teste', teste);
-app.use('/resenha', resenha);
-app.use('/quiz', quizController);
+app.use('apis/resenha', resenha);
+app.use('apis/quiz', quizController);
 
 // usa configuracoes
 mongoose.connect(db.url);
