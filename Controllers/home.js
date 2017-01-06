f570839153013fd64b2d2af2e956bcbbaa57aa6c
enzotiezzi <enzo_tiezzi@hotmail.com>
2017-01-06 03:13:11 -0200

    a

--- Controllers/home.js
+++ Controllers/home.js
@@ -2,8 +2,5 @@ var express = require('express');
 var views = require('../config/front');
 var router = express.Router();
 
-router.get('/', function (req, res) {
-	res.sendFile('index.html', { root: views.path });
-});
 
 module.exports = router;
