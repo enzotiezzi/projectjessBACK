3d514d810f8626653f421365e1ef2898c1d89290
enzotiezzi <enzo_tiezzi@hotmail.com>
2017-01-07 20:27:56 -0200

    tirando tudo

--- Controllers/home.js
+++
@@ -1,6 +0,0 @@
-var express = require('express');
-var views = require('../config/front');
-var router = express.Router();
-
-
-module.exports = router;
--- config/front.js
+++
@@ -1,5 +0,0 @@
-var path = require('path');
-
-var front_path = path.join(__dirname, '../public/views/');
-
-module.exports = { path: front_path };
