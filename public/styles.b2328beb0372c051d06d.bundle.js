879fae9dea25ff9e4f39adb289cf167da9cc8b8a
enzotiezzi <enzo_tiezzi@hotmail.com>
2017-01-07 17:28:49 -0200

    sidebar

--- public/styles.b2328beb0372c051d06d.bundle.js
+++ public/styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([2,4],{395:function(n,o){},735:function(n,o,c){n.exports=c(395)}},[735]);
+webpackJsonp([2,4],{472:function(n,o){},827:function(n,o,c){n.exports=c(472)}},[827]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
